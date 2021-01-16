f820f8ba14456d47ca5a06064abfa60ec48c9d34
Hung Vu <hunghvu2017@gmail.com>
2021-01-16 00:21:37 -0800

    Capture position when moving and clicking mouse

--- source/js/controller/controls.js
+++ source/js/controller/controls.js
@@ -6,7 +6,6 @@ class Controls {
     this.registerKey('right', 'KeyD');
     this.registerKey('enter', 'Enter');
     this.registerKey('jump', 'Space');
-    this.registerKey('jump', 'Space');
     // How much the scroll wheel has been scrolled since last checked
     this.scrollDelta = 0;
 
@@ -14,6 +13,21 @@ class Controls {
     window.addEventListener('wheel', event => {
       this.scrollDelta += event.deltaY ;
     });
+
+    this.clickX = 0;
+    this.clickY = 0;
+    this.moveX = 0;
+    this.moveY = 0;
+    window.addEventListener('click', event => {
+      this.clickX = event.clientX;
+      this.clickY = event.clientY;
+    })
+
+    window.addEventListener('mousemove', event => {
+      this.moveX = event.clientX;
+      this.moveY = event.clientY;
+      // console.log (this.moveX + ' ' + this.moveY);
+    })
   }
 
   // Call this function after every update.
