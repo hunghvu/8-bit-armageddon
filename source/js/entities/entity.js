daad0279bb7c9c06d8e7a54536a73747f184feef
danieyll <danieyll@danny.wilsons>
2021-01-17 13:53:07 -0800

    added drawEntity function to entity.js, remove drawing entity inside world which should now call the drawEntity function instead

--- source/js/entities/entity.js
+++ source/js/entities/entity.js
@@ -1,11 +1,17 @@
 class Entity extends Rectangle {
   constructor(spriteSheet, x, y) {
     super(x - 3, y, 6, 48);
-    
+
     this.vel = new Point(0, 0);
     this.acc = new Point(0, 0);
-    
+
     this.onGround = false;
+  }
+
+  drawEntity(ctx)
+  {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 
   setAcceleration(newAcc) {
@@ -21,7 +27,7 @@ class Entity extends Rectangle {
     // Update the acceleration
     this.vel.x += this.acc.x;
     this.vel.y += this.acc.y;
-    
+
     return new Point(this.vel.x, this.vel.y);
   }
 }
