232dc75214e001e865e7278246ad1ecef1bf12b2
Hung Vu <hunghvu2017@gmail.com>
2021-01-20 22:43:29 -0800

    Create an 'object' represents shooting angles

---
+++ source/js/dataStructures/shootingAngle.js
@@ -0,0 +1,39 @@
+/*
+    This presents a shooting angle of a specific weapon.
+    Think of a circle sector (fan) shape.
+        The origin points should be at player position.
+        The radius is the length of indicator (line).
+        The bounds indicates range of usable angles, relative to
+            player facing angle - Hung Vu.
+*/
+class shootingAngle {
+    // The points may or not be able to be constructed using point.js 
+    //  because the transformation may not be linear. For now, we will
+    //  directly assign them for developing purpose.
+    constructor (originX, originY, radius, lowerBound, uperBound, defaultAngle) {
+        Object.assign(this, {originX, originY, radius, lowerBound, uperBound, defaultAngle})
+
+        // These are points on the circumference.
+        this.endPointX = 0;
+        this.endPointY = 0;
+    }
+
+    updateOrigin(newX, newY) {
+        this.originX = newX;
+        this.originY = newY;
+    }
+
+    updateEndPoints(newX, newY) {
+        this.endPointX = newX;
+        this.endPointY = newY;
+    }
+
+    increaseAngle() {
+        defaultAngle++;
+    }
+
+    decreaseAngle() {
+        defaultAngle--;
+    }
+
+}
