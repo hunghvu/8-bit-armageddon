3f9c31128a26c9f204aef6550a4cc3e572e2aa14
Hung Vu <hunghvu2017@gmail.com>
2021-01-25 22:28:12 -0800

    Temporary turn switching that turns stop when player lands on ground

--- source/js/dataStructures/Turn.js
+++ source/js/dataStructures/Turn.js
@@ -15,19 +15,44 @@ class Turn {
         //  Delay 3000ms to load the UI as a work around.
         setTimeout(() => {
             this.timer = setInterval(() => {
-            if (this.playerNumber === -1) {
-                this.playerNumber = this.world.players.length - 1;
-            } 
-            // this.game.controls.endOfTurn();
-            // this.world.currentPlayer[this.playerNumber].velocity(0);
-            this.world.currentPlayer.isInTurn = false;
-            this.world.currentPlayer = this.world.players[this.playerNumber]; 
-            this.world.currentPlayer.isInTurn = true;
-            // this.world.waitingPlayers.push(this.world.players.splice(this.playerNumber, 1));
-            console.log(new Date())
-            // console.log(true);
-            this.playerNumber--;
-            }, this.timePerTurn)
+                if (this.playerNumber === -1) {
+                    this.playerNumber = this.world.players.length - 1;
+                } 
+                
+                // This only create a copy of current player, not pointer (?)
+                // let currentPlayer = this.world.currentPlayer; 
+                // console.log(currentPlayer.onGround);
+                // if(!this.world.currentPlayer.onGround) {
+                //     // console.log(true);
+                //     return;
+                // }
+                // console.log(false);
+                while(!this.world.currentPlayer.onGround) {
+                    this.world.update(this.game.timer.tick(), this.game.controls);
+                    // this.world.draw(this.game.ctx, this.game.canvas.width, this.game.canvas.height);
+                    // this.game.draw();
+                    // console.log(currentPlayer.onGround);d
+                    // console.log(currentPlayer.isInTurn);
+                }
+                // console.log(true);
+                // if(currentPlayer.onGround) {
+                // if(currentPlayer.onGround) {
+        
+                    this.world.currentPlayer.isInTurn = false;
+                    this.world.currentPlayer = this.world.players[this.playerNumber]; 
+                    this.world.currentPlayer.isInTurn = true;
+        
+                console.log(new Date())
+                // console.log(true);
+                this.playerNumber--;
+                // }
+                // console.log(new Date())
+            }, this.timePerTurn);
         }, 3000);
     }
-}
+
+    // Not rerun when extracting task in time interval (?)
+    // updateTurn() {
+
+    // }
+}   
