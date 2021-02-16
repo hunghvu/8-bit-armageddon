bd45c08b5c9330ac20e683f668128d88d4edde93
Hung Vu <hunghvu2017@gmail.com>
2021-02-15 23:44:19 -0800

    Comment out redundant code, fix UI overlaping due to merge, move player to surface

--- source/js/Game.js
+++ source/js/Game.js
@@ -226,11 +226,11 @@ class Game {
 
       this.ctx.font = "30px Arial";
       // this.ctx.fillText("Wind(X): " + Wind.x + ", Wind(Y): " + Wind.y, 343, 31);
-      this.ctx.fillText("Wind(X): " + Wind.x + ", Wind(Y): " + Wind.y, 455, 31);
+      this.ctx.fillText("Wind(X): " + Wind.x + ", Wind(Y): " + Wind.y, 465, 31);
       
       let turnIteration = [];
       this.world.players.forEach(element => turnIteration.push(element.playerNo));
-      this.ctx.fillText("Turn iteration (player No.): " + turnIteration, 343, 70);
+      this.ctx.fillText("Turn iteration (player No.): " + turnIteration, 465, 70);
 
       // For testing only.
       // this.ctx.font = "30px Arial";
