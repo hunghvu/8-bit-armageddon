902451acf6b470f717d6a8a7fb2720d927c9a12c
danieyll <danieyll@danny.wilsons>
2021-02-18 19:28:09 -0800

    added grenadeLevel2 class

--- source/js/entities/weapons/Grenade.js
+++ source/js/entities/weapons/Grenade.js
@@ -1,34 +1,43 @@
-class GrenadeLauncherLevel2 extends GrenadeLauncher{
+/**
+ * An upgraded weapon, the Grenade Launcher, can be spawned from an item crate.
+ */
+class Grenade extends Projectile{
     /**
      * Constructor for the grenade laucher that extends the entity class.
      */
-    constructor(x,y,angle,power) {
-        super(x,y,angle,power);
+    constructor(x, y, angle, power) {
+      super(x, y, angle, power);
+      this.projectileCanEndTurn = false; // It is not used as of now.
+      this.spritesheet = MANAGER.getAsset('./assets/weapons.png');
 
-        this.spritesheet = MANAGER.getAsset('./assets/weapons.png');
-
-        this.animations = [];
-        this.loadAnimations();
-
-        this.projectileCanEndTurn = false; // It is not used as of now.
+      this.animations = [];
+      this.loadAnimations();
     }
 
+    /**
+     * Update the bullet flying through the air.
+     *
+     * @params {World} - The world object that should be referenced
+     * @params {deltaT} - The number of ms since the last update
+     */
     update(world, deltaT){
-        this.add(this.desiredMovement(deltaT))
-
         // update direction/facing
         if (this.vel.x < 0) this.facing = 1;
         if (this.vel.x > 0) this.facing = 0;
 
-        this.add(this.desiredMovement(deltaT, Wind.x, Wind.y))
-
+        this.moveUntilCollision(world, this.desiredMovement(deltaT, Wind.x, Wind.y));
         if (world.map.collideWithRectangle(this)) {
+            // Destroy this bullet if we hit something
             this.active = false;
             this.projectileCanEndTurn = true;
-            world.map.destroyCircle(this.center.x, this.center.y, 60);
+            //let destructionRect = new Rectangle(this.x, this.y, 20, 20);
+            //destructionRect.center = this.center;
+            //world.map.destroyRectangle(destructionRect);
+            world.map.destroyCircle(this.center.x, this.center.y, 20);
             // Find any players in the blast range
             for (let i = 0; i < world.players.length; i++) {
                 let playerThisLoop = world.players[i];
+                console.log(playerThisLoop);
                 // If we are close enough then damage a player
                 let difference = playerThisLoop.center
                 difference.sub(this.center);
@@ -55,12 +64,12 @@ class GrenadeLauncherLevel2 extends GrenadeLauncher{
       for (var j = 0; j < 2; j++) { //facing
         this.animations.push([]);
       }
-      // Dynomite (upgrade lvl 2) = no set number
-      //buffer padding current build =
+      // Grenade = no set number
+      //buffer padding current build = 17
       //facing right = 0,
-      this.animations[0] = new Animator(this.spritesheet, 2, 35, 28, 28, 4, 0.5, 14, false, true);
+      this.animations[0] = new Animator(this.spritesheet, 9, 7, 12, 14, 4, 0.5, 17, false, true);
 
       //facing left = 1,
-      this.animations[1] = new Animator(this.spritesheet, 2, 35, 28, 28, 4, 0.5, 14, true, true);
+      this.animations[1] = new Animator(this.spritesheet, 137, 7, 12, 14, 4, 0.5, 17, true, true);
     }
 }
