0da192493fb1408dbef898516347342c361732a9
Hung Vu <hunghvu2017@gmail.com>
2021-03-03 17:46:09 -0800

    Add groundwork to build shooting power

---
+++ source/js/world/ShootingPower.js
@@ -0,0 +1,31 @@
+/**
+ * This class will provide shooting power for the weapons
+ */
+class ShootingPower {
+
+    constructor () {
+        // Should be empty. Only used this class for static var.
+    }
+    
+    static max = 1000; // The max and min winds are just arbitrary number now. 
+    static min = 0; //  They should be fixed values determined by game creators.
+    static power = 0;
+    static direction = true; // True means increasing, false otherwise. Start decreasing when value reaches 1000.
+
+    /**
+     * Logic to manipulate shooting power.
+     * Increase power until it reach 1000 then gradually reduce back to 0.
+     */
+    static change() {
+        if (this.power >= this.max) this.direction = false;
+        this.direction ? this.power += 2 : this.power -= 2;
+    }
+
+    /**
+     * Reset the values when starting a new turn.
+     */
+    static reset() {
+        this.power = 0;
+        this.direction = false;
+    }
+}
