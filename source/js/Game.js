654a22070fd9aada8471a66e6657c49c70fba319
DewWee05 <68929628+DewWee05@users.noreply.github.com>
2021-03-09 13:49:52 -0800

    Fix the display bar on top left to be top center, change the font color of the end game/pause menu.

--- source/js/Game.js
+++ source/js/Game.js
@@ -64,17 +64,17 @@ class Game {
       // console.log(this.canvas.width);
       // this.ctx.fillRect(0,0,this.canvas.width,100);
       this.spritesheet = MANAGER.getAsset('./assets/DisplayBar.png');
-      this.ctx.drawImage(this.spritesheet, 0, 0, 1536, 100, 0, 0, 1536, 100);
+      this.ctx.drawImage(this.spritesheet, 0, 0, 1536, 100, 0, 0, this.canvas.width, 100);
 
 
       this.ctx.fillStyle = "Black";
 
       this.ctx.font = "16px 'Press Start 2P'";
-      this.ctx.fillText('Timer: ', 60, 31);
+      this.ctx.fillText('Timer: ', 1400, 31);
       this.ctx.font = "30px 'Press Start 2P'";
       this.timer.turnTime < 0
-      ? this.ctx.fillText(Math.round(this.timer.turnTime * -1), 95, 75) // Different print text fill method for the first ready period.
-      : this.ctx.fillText(this.timePerTurnLimit - Math.round(this.timer.turnTime % this.timePerTurnLimit), 100, 75);
+      ? this.ctx.fillText(Math.round(this.timer.turnTime * -1), 1430, 75) // Different print text fill method for the first ready period.
+      : this.ctx.fillText(this.timePerTurnLimit - Math.round(this.timer.turnTime % this.timePerTurnLimit), 1420, 75);
 
       this.ctx.font = "16px 'Press Start 2P'";
       this.ctx.fillText('Weapon: ', 175, 31);
@@ -89,27 +89,27 @@ class Game {
         //Sniper
         else if (this.world.currentPlayer.currentWeapon.myCurrentWeapon == Sniper) {
           this.ctx.drawImage(this.spritesheet, 191, 37, 33, 15, 180, 35, 99, 45);
-          this.ctx.fillText('Sniper DMG: 30', 180, 93);
+          this.ctx.fillText('Sniper DMG: 20', 180, 93);
         }
         // Laser
         else if (this.world.currentPlayer.currentWeapon.myCurrentWeapon == Laser) {
           this.ctx.drawImage(this.spritesheet, 224, 37, 32, 19, 190, 35, 76, 45);
-          this.ctx.fillText('Laser DMG: 20', 180, 93);
+          this.ctx.fillText('Laser DMG: 25', 180, 93);
         }
         // Grenade
         else if (this.world.currentPlayer.currentWeapon.myCurrentWeapon == Grenade) {
           this.ctx.drawImage(this.spritesheet, 10, 7, 11, 14, 200, 35, 35, 45);
           this.ctx.fillText('Grenade DMG: 15', 180, 93);
         }
-        // Dynomite
+        // Dynamite
         else if (this.world.currentPlayer.currentWeapon.myCurrentWeapon == GrenadeLevel2) {
           this.ctx.drawImage(this.spritesheet, 2, 35, 28, 28, 200, 35, 33, 45);
-          this.ctx.fillText('Dynomite DMG: 30', 180, 93);
+          this.ctx.fillText('Dynamite DMG: 30', 180, 93);
         }
         // Rocket
         else if (this.world.currentPlayer.currentWeapon.myCurrentWeapon == GrenadeLevel3) {
           this.ctx.drawImage(this.spritesheet, 130, 261, 27, 20, 200, 35, 61, 45);
-          this.ctx.fillText('Rocket DMG: 50', 180, 93);
+          this.ctx.fillText('Rocket DMG: 45', 180, 93);
         }
         //PortalGun
         else if (this.world.currentPlayer.currentWeapon.myCurrentWeapon == PortalGun) {
@@ -208,7 +208,7 @@ class Game {
     ctx.save();
     ctx.textAlign = "center";
     ctx.textBaseline = "top";
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "red";
     let teamNumer = this.forfeitCode + 1;
     ctx.fillText("Team " + teamNumer + " want to forfeit.", ctx.canvas.width / 2, ctx.canvas.height / 2);
     ctx.fillText("Press Y to vote Yes, and Esc to cancel.", ctx.canvas.width / 2, ctx.canvas.height / 3 * 2);
@@ -220,7 +220,7 @@ class Game {
     ctx.save();
     ctx.textAlign = "center";
     ctx.textBaseline = "top";
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "red";
     ctx.fillText("MATCH ENDED", ctx.canvas.width / 2, ctx.canvas.height / 2);
     if (this.endCode === 1 || this.endCode === 2) {
       ctx.fillText("Team " + this.endCode + " won!", ctx.canvas.width / 2, ctx.canvas.height / 3 * 2);
@@ -234,7 +234,7 @@ class Game {
     ctx.save();
     ctx.textAlign = "center";
     ctx.textBaseline = "top";
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "red";
     ctx.fillText("PAUSED", ctx.canvas.width / 2, ctx.canvas.height / 2);
     ctx.restore();
   }
